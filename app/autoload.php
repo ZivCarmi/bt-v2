b9770d5cd2b8f885727ade583cc76f1292542bf2
Victor Cooper <62182843+buttered-toast@users.noreply.github.com>
2020-10-17 13:20:29 +0300

    Update autoload.php

--- app/autoload.php
+++ app/autoload.php
@@ -1,9 +1,9 @@
 <?php if (!defined('ABSPATH')) {exit;}
 
 spl_autoload_register(function ($class) {
-	$file = THEME_DIR . $class . '.class.php';
+	$file = str_replace('\\', '/', THEME_DIR . $class . '.class.php');
 
 	if (file_exists($file)) {
-		require str_replace('\\', '/', $file);
+		require $file;
 	}
 });
