8153c0fcdd67f1e8ef5127740df1193caefc44d0
Victor Cooper <62182843+buttered-toast@users.noreply.github.com>
2020-10-17 13:23:36 +0300

    Update Actions.class.php

--- app/classes/plugins/woocommerce/Actions.class.php
+++ app/classes/plugins/woocommerce/Actions.class.php
@@ -5,13 +5,24 @@ if (!defined('ABSPATH')) {exit;}
 
 class Actions {
 	static public function init () {
-		// dequeue default woocommerce styles
-		add_action( 'wp_enqueue_scripts', [__NAMESPACE__  . '\Actions', 'bt_dequeue_woocommerce_styles'] );
+		// dequeue default woocommerce styles/scripts
+		add_action('wp_enqueue_scripts', [__NAMESPACE__  . '\Actions', 'bt_dequeue_woocommerce_styles']);
+		add_action('wp_enqueue_scripts', [__NAMESPACE__  . '\Actions', 'bt_dequeue_woocommerce_scripts']);
 	}
 	
 	static public function bt_dequeue_woocommerce_styles () {
-		if (TRUE) {
+		if (!is_admin()) {
 			wp_dequeue_style('wc-block-style');
+		}
+	}
+	
+	static public function bt_dequeue_woocommerce_scripts () {
+		if (!is_admin()) {
+			wp_dequeue_script('jquery-blockui');
+			wp_dequeue_script('wc-add-to-cart');
+			wp_dequeue_script('js-cookie');
+			wp_dequeue_script('woocommerce');
+			wp_dequeue_script('wc-cart-fragments');
 		}
 	}
 }
