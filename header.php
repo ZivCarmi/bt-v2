e9ee3409cca8660dddfbb6fa05d99d9385764468
Victor Cooper <62182843+buttered-toast@users.noreply.github.com>
2020-10-24 11:32:10 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,9 +1,15 @@
 <?php if (!defined('ABSPATH')) {exit;} ?><!doctype html>
 <html <?php language_attributes(); ?>>
 	<head>
+		<?php do_action('bt_html_head_start'); ?>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="icon" href="data:,">
-		<?php wp_head(); ?>
+		<?php
+		wp_head();
+		
+		do_action('bt_html_head_end');
+		?>
 	</head>
 	<body <?php body_class(); ?>>
+		<?php do_action('bt_html_body_start'); ?>
