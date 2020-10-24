2857fa71b73a4e0638dc6b9146cd6c6bcbe0f46b
Victor Cooper <62182843+buttered-toast@users.noreply.github.com>
2020-10-24 11:31:22 +0300

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,7 +1,13 @@
 <?php if (!defined('ABSPATH')) {exit;}
 
 get_header();
-?>
-<main>
-</main>
-<?php get_footer(); ?>
+
+if (have_posts()) {
+	while (have_posts()) {
+		the_post();
+
+		the_content();
+	}
+}
+
+get_footer();	
